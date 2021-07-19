a78e4a06e859f344c15b91fddee88ac619edafc3
amirpeace1080 <amirpeace1080@gmail.com>
2021-07-19 16:26:03 +0430

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,20 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    user: null,
+  },
+  mutations: {
+    SET_USER(state, userData) {
+      state.user = userData;
+      localStorage.setItem('users', JSON.stringify(userData))
+    }
+  },
+  actions: {
+    // saveData(name, family) {
+    //   localStorage.setItem(name, family)
+    // }
+  },
   modules: {},
+  getter:{}
 });
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <form>
+      <input type="text" value="NAME" v-model="name">
+      <input type="text" value="FAMILY" v-model="family">
+      <br />
+      <button @click="saveBtn">SAVE</button>
+    </form>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: null,
+      family: null,
+    }
+  },
+
+  methods: {
+    saveBtn(){
+      let name = this.name;
+      let family = this.family;
+      this.$store.commit("SET_USER", {name, family});
+    }
+  },
+}
+</script>
